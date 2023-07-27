dc1623599be766528fe7f76157cc548659b03cd9
juste.noreikaite <juste.noreikaite>
2023-07-27 10:03:53 +0300

    Remove mock data

--- front-end/index.js
+++ front-end/index.js
@@ -8,15 +8,6 @@ import {
 import { showLoader, hideLoader } from './src/loader/index.js'
 import { getRepaymentSchedule } from './src/api.js'
 
-const data = [
-  { remaining: 10052.27, principal: 1947.73, interest: 127.0, total: 2074.73 },
-  { remaining: 8083.93, principal: 1968.34, interest: 106.39, total: 2074.73 },
-  { remaining: 6094.75, principal: 1989.18, interest: 85.55, total: 2074.73 },
-  { remaining: 4084.52, principal: 2010.23, interest: 64.5, total: 2074.73 },
-  { remaining: 2053.02, principal: 2031.5, interest: 43.23, total: 2074.73 },
-  { remaining: 0.0, principal: 2053.02, interest: 21.73, total: 2074.73 },
-]
-
 const FORM_ID = 'amount-form'
 const TABLE_ID = 'schedule-table'
 
