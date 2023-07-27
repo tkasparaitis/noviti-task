499fc697a1fb5a0c3167548fcf3a26a15fb4619e
juste.noreikaite <juste.noreikaite>
2023-07-27 14:10:50 +0300

    Extract schedule calculation to a different function

--- rest-api/src/Controller/NovitiController.php
+++ rest-api/src/Controller/NovitiController.php
@@ -13,19 +13,35 @@ class NovitiController extends AbstractController
     #[Route('/noviti', name: 'app_noviti')]
     public function index(Request $request): JsonResponse
     {
-        $yearlyInterest = 12.7;
-        $termMonths = 6;
         $totalAmount = $request->query->get('totalAmount');
 
+        $loanSchedule = $this->generateRepaymentSchedule($totalAmount);
+
+        $response = $this->json([
+            'schedule' => $loanSchedule
+        ]);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+
+        return $response;
+    }
+    
+    private function generateRepaymentSchedule($totalAmount) 
+    {
+        $yearlyInterest = 12.7;
+        $termMonths = 6;
+        
         $monthlyInterestRate = ($yearlyInterest / 100) / 12;
         $annuityPayment = round($totalAmount * ($monthlyInterestRate * pow(1 + $monthlyInterestRate, $termMonths))
                       / (pow(1 + $monthlyInterestRate, $termMonths) - 1), 2);
         
         $remainingCredit = $totalAmount;
         $totalPayment = 0;
-
+    
         $loanSchedule = array();
-
+    
         for ($month = 1; $month <= $termMonths; $month++) {
             $interest = round($remainingCredit * $monthlyInterestRate, 2);
             $principalPart = round($annuityPayment - $interest, 2);
@@ -41,15 +57,7 @@ class NovitiController extends AbstractController
         
             $loanSchedule[] = $paymentObj;
         }
-
-        $response = $this->json([
-            'schedule' => $loanSchedule
-        ]);
-
-        $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
-
-        return $response;
+    
+        return $loanSchedule;
     }
 }
